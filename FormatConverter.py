f34c946dec109c8eb4013a50bd35638f921cd735
Jonathan <jonathan.spirit@yahoo.com>
2019-07-19 14:20:53 -0700

    Better accepting string generator usage Parsing ans.dot

--- FormatConverter.py
+++ FormatConverter.py
@@ -1,3 +1,5 @@
+import re
+
 # http://ivanzuzak.info/noam/webapps/fsm_simulator/
 def printNoam(tensor, file="noam.txt"):
     states = ['s' + str(i) for i in range(tensor.state)]
@@ -28,3 +30,54 @@ def printNoam(tensor, file="noam.txt"):
     noamFile.writelines(['#transitions\n'])
     for i in transitions:
         noamFile.writelines([i,'\n'])
+
+def parseDot(file="ans.dot"):
+    ansFile = open(file, 'r')
+    ansString = ansFile.read()
+    ansStringList = ansString.split('\n')
+    initState = 0
+    accStates = set()
+    stateDict = {}
+    symbolDict = {}
+    takenStates = 0
+    takenSymbols = 0
+    delta = []
+    for stringToTest in ansStringList:
+        #Check if it represents an initial state.
+        if stringToTest.__contains__('bold'):
+            startState = int(re.findall('[0-9][0-9]*', stringToTest)[0])
+            if not stateDict.keys().__contains__(startState):
+                stateDict[startState] = takenStates
+                delta.append([0 for i in range(takenSymbols)])
+                takenStates += 1
+            initState = stateDict[startState]
+        #Check if it represents an accepting state.
+        if stringToTest.__contains__('peripheries'):
+            endState = int(re.findall('[0-9][0-9]*', stringToTest)[0])
+            if not stateDict.keys().__contains__(endState):
+                stateDict[endState] = takenStates
+                delta.append([0 for i in range(takenSymbols)])
+                takenStates += 1
+            accStates.add(stateDict[endState])
+        #Check if it is a transition.
+        if stringToTest.__contains__('->'):
+            transitionList = re.findall('[0-9][0-9]*|"."', stringToTest) # Get in state, out state, and character.
+            startState = int(transitionList[0])
+            endState = int(transitionList[1])
+            symbol = transitionList[2][1]
+            if not stateDict.keys().__contains__(startState):
+                stateDict[startState] = takenStates
+                delta.append([0 for i in range(takenSymbols)])
+                takenStates += 1
+            if not stateDict.keys().__contains__(endState):
+                stateDict[endState] = takenStates
+                delta.append([0 for i in range(takenSymbols)])
+                takenStates += 1
+            if not symbolDict.keys().__contains__(symbol):
+                symbolDict[symbol] = takenSymbols
+                takenSymbols += 1
+                for row in delta:
+                    row.append(0)
+            delta[stateDict[startState]][symbolDict[symbol]] = stateDict[endState]
+
+    return initState, accStates, delta
